c065dbe4f4cd471876ccfc58c24113133e3c7e92
nvi9 <42123156+nvi9@users.noreply.github.com>
2022-03-12 21:29:10 +0100

    AssetCategory és AssetRequest serializerek

--- joladnijo/serializers.py
+++ joladnijo/serializers.py
@@ -17,10 +17,27 @@ class OrganizationSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class AssetCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.AssetCategory
+        fields = '__all__'
+
+
+class AssetRequestSerializer(serializers.ModelSerializer):
+    category = AssetCategorySerializer()
+
+    class Meta:
+        model = models.AssetRequest
+        exclude = ['aid_center']
+
+
 class AidCenterSerializer(serializers.ModelSerializer):
     organization = OrganizationSerializer()
     contact = ContactSerializer()
     geo_location = serializers.JSONField()
+    assets_requested = AssetRequestSerializer(many=True, read_only=True)
+    assets_fulfilled = AssetRequestSerializer(many=True, read_only=True)
+    assets_overloaded = AssetRequestSerializer(many=True, read_only=True)
 
     class Meta:
         model = models.AidCenter

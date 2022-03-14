8b8798e758dfc65be18050061680da51d92bb7c4
Misu <mihaly.szijjartonagy@prezi.com>
2022-03-14 23:05:06 +0100

    GET /aid-centers/{slug} endpoint létrehozása (#27)
    * starting to create models
    
    * Engine váltás GeoDjango Database API-hoz
    
    * Package telepítések
    
    * AidCenter geo_location visszaállítása
    
    * Mező típus módosítások
    
    * Address mezők áthelyezése AidCenterhez, migrációk összevonása
    
    * django.contrib.gis hozzáadása INSTALLED_APPS-hoz
    
    * Django admin model regisztrálások
    
    * REST API serializerek, view-ok, urlek
    
    * Model __str__ definíciók django admin lista nézethez
    
    * Adatbázis utf8mb4 karakterkészlettel fusson, Django nyelv és TZ beállítása
    
    * Egymásba ágyazott serializerek
    
    * Nem feltétlenül egy soros szöveges mezők típusának módosítása
    
    * Admin felület csinosítgatások
    
    * REST_FRAMEWORK config
    
    * AidCenter endpoint a default id helyett slug alapján azonosít
    
    * Váltás camelCase rendererre és parserre
    
    * AidCenter geo_location kezelése JSON formátumban
    
    * SimpleHistory előzmények kezelésére; AssetCategory és AssetRequest modelek hozzáadása
    
    * AssetCategory és AssetRequest model kiegészítések
    
    * AssetCategory és AssetRequest serializerek
    
    * AssetCategory és AssetRequest admin
    
    * AssetCategory ne lehessen önmaga szülője
    
    * AidCenter feed
    
    * AssetCategory __str__ szülő kategória nevével (ha van)
    
    * Szülővel rendelkező AssetCategory-t ne lehessen kiválasztani szülőként admin felületen
    
    * Organization és AidCenter contact inline form
    
    * AidCenter feed javítások
    
    Co-authored-by: nvi9 <42123156+nvi9@users.noreply.github.com>

--- dev.Dockerfile
+++ dev.Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.9
 
 COPY requirements.txt ./
 RUN apt-get update \
-    && apt-get install python3-dev -y \
+    && apt-get install python3-dev binutils libproj-dev gdal-bin -y \
     && pip install --upgrade pip \
     && pip install -r requirements.txt
 
--- joladnijo/settings.py
+++ joladnijo/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'rest_framework',
+    'simple_history',
+    'joladnijo',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'joladnijo.urls'
@@ -81,12 +85,13 @@ if not db_password:
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.contrib.gis.db.backends.mysql',
         'NAME': 'joladnijo',
         'USER': 'svc_backend',
         'PASSWORD': db_password,
         'HOST': os.environ.get('DBHOST', 'localhost'),
         'PORT': os.environ.get('DBPORT', '3306'),
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
@@ -113,9 +118,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'hu-hu'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Budapest'
 
 USE_I18N = True
 
@@ -133,3 +138,18 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',  # TODO: törölni majd
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+}
+
+SIMPLE_HISTORY_FILEFIELD_TO_CHARFIELD = True

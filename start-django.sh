435ecda3c23e987ea37ca5d4bdff3217c9aa7f9b
Molnár Bence <xjarekx@gmail.com>
2022-04-04 18:35:36 +0200

    Fix boostrap shell script

--- start-django.sh
+++ start-django.sh
@@ -4,11 +4,8 @@ python manage.py check_db
 python manage.py migrate
 
 if [ "$ENVIRONMENT" = 'development' ] || [ "$ENVIRONMENT" = 'staging' ]
-    python manage.py runserver
 then
     python manage.py loaddata seeds/default.yaml
 fi
 
 python manage.py runserver 0.0.0.0:8000
-
-if 
